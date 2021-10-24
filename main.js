f5c5a25af3dde23ca9381af36003893b1c74052e
Ushlerdop <axalimon@gmail.com>
2021-10-24 19:43:14 +1000

    homework-6
    Привет! Сделал :)

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const $arenas = document.querySelector('.arenas');
 const $fightButton = document.querySelector('.control .button');
 const $loseTitle = createElement('div', 'loseTitle');
 const $formFight = document.querySelector('.control');
+const $chat = document.querySelector('.chat');
+
+const date = new Date();
 
 const HIT = {
     head: 30,
@@ -82,6 +85,7 @@ function getRandom(max) {
   }
 
 function checkTheWinner(firstPlayer, secondPlayer) {
+    let winner = '';
 
     if (firstPlayer.hp === 0 || secondPlayer.hp === 0) {
         $fightButton.disabled = true;        
@@ -89,15 +93,18 @@ function checkTheWinner(firstPlayer, secondPlayer) {
     }
 
     if (firstPlayer.hp === 0) {        
-        $arenas.appendChild(playerWon(secondPlayer.name));    
+        $arenas.appendChild(playerWon(secondPlayer.name));
+        return winner = '2';
     }
 
     if (secondPlayer.hp === 0) {
         $arenas.appendChild(playerWon(firstPlayer.name));
+        return winner = '1';
     }
 
     if (firstPlayer.hp === 0 && secondPlayer.hp === 0) {
         $loseTitle.innerText = `It's a draw`;
+        return winner = 'draw';
     }
 }
 
@@ -120,7 +127,7 @@ function elHP() {
 }
 
 function renderHP() {
-    this.elHP().style.width = this.hp + '%';
+    this.elHP().style.width = `${this.hp}%`;
 }
 
 function playerTurn(player, damage) {
@@ -157,10 +164,7 @@ function enemyAttack() {
     }
 }
 
-$formFight.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const enemy = enemyAttack();
-
+function playerAttack() {
     const attack = {};
 
     for (item of $formFight) {
@@ -174,13 +178,54 @@ $formFight.addEventListener('submit', function(e) {
         item.checked = false;
     }
 
-    console.log('#### МЫ',attack);
-    console.log('!!!! ИИ',enemy);
-    if (enemy.hit !== attack.defence) {
-        playerTurn(player2, enemy.value);
+    return attack;
+}
+
+function generateLogs(type, player1, player2, playerValue) {
+    let text = '';
+    switch (type) {
+        case 'hit':
+            text = `${date.toLocaleString().slice(12,20)} ${logs[type][getRandom(18) - 1].replace('[playerKick]', player1.name).replace('[playerDefence]', player2.name)} -${playerValue.value} [${player2.hp}/100]`;
+            break;
+        case 'start':
+            text = logs[type].replace('[time]', `${date.toLocaleString().slice(12,20)}`).replace('[player1]', player1.name).replace('[player2]', player2.name);
+            break;
+        case 'end':
+            text = logs[type][getRandom(3) - 1].replace('[playerWins]', player1.name).replace('[playerLose]', player2.name);
+            break;
+        case 'draw':
+            text = logs[type];
+            break;
     }
-    if (attack.hit !== enemy.defence) {        
-        playerTurn(player1, attack.value);
+
+    const el = `<p>${text}</p>`;
+    $chat.insertAdjacentHTML('afterbegin', el);
+}
+
+$formFight.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const enemy = enemyAttack();
+    const player = playerAttack();
+    if (enemy.hit !== player.defence) {
+        playerTurn(player2, enemy.value);
+        generateLogs('hit', player1, player2, enemy);
+    }
+    if (player.hit !== enemy.defence) {        
+        playerTurn(player1, player.value);
+        generateLogs('hit', player2, player1, player);
     }
     checkTheWinner(player1, player2);
+    switch (checkTheWinner(player1, player2)) {
+        case '1':
+            generateLogs('end', player1, player2);
+            break;
+        case '2':
+            generateLogs('end', player2, player1);
+            break;
+        case 'draw':
+            generateLogs('draw', player1, player2);
+            break;
+    }
 })
+
+generateLogs('start', player1, player2);
