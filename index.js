3a1d91b146e527bb1d78783532f879f590ba3b02
Ushlerdop <axalimon@gmail.com>
2021-11-02 15:56:30 +1000

    Update index.js

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ async function init() {
                 // TODO: Здесь должен быть код который перенаправит вас на ваше игровое поле...
                 //  Пример использования: window.location.pathname = 'arenas.html';
                 window.location.pathname = 'arena.html';
-            }, 3900);          
+            }, 4300);          
         });
 
         img.src = item.avatar;
